290ab5c508eac3251cedf3110b4aee92cc3da7d9
Nicole Bartholomew <nabartholomew@gmail.com>
2019-09-17 10:06:18 -0500

    (feat) Completed initial solution for times_in_list.

--- cs-challenges-master/problems/recursion/times_in_list.js
+++ cs-challenges-master/problems/recursion/times_in_list.js
@@ -8,11 +8,26 @@
 (function(CSC){
 
 CSC.iterativeTimes = function(arr, element) {
-
+  let count = 0;
+  for(let el of arr) {
+    if(arr[0] === element) {
+      count++
+    }
+  }
+  return count;
 };
 
-CSC.recursiveTimes = function(arr,element) {
-  
+CSC.recursiveTimes = function(arr, element) {
+  if(arr.length === 0) {
+    return 0;
+  }
+  let el = arr.shift();
+  if(el === element) {
+    return CSC.recursiveTimes(arr, element) + 1;
+  }
+  else {
+    return CSC.recursiveTimes(arr, element);
+  }
 };
 
 })(CSC);
