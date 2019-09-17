8beefa31dc7a540bfca719579396191281b3060b
Nicole Bartholomew <nabartholomew@gmail.com>
2019-09-17 10:32:48 -0500

    (feat) Completed initial solution for order_reverse.

--- cs-challenges-master/problems/recursion/order_reverse.js
+++ cs-challenges-master/problems/recursion/order_reverse.js
@@ -8,11 +8,20 @@
 (function(CSC){
 
 CSC.iterativeReverse = function(arr) {
-
+  for(let i = 0; i <= Math.floor(arr.length / 2); i++) {
+    let temp = arr[i];
+    arr[i] = arr[arr.length - i - 1];
+    arr[arr.length - i - 1] = temp;
+  }
+  return arr;
 };
 
 CSC.recursiveReverse = function(arr) {
-
+  if(arr.length === 1) {
+    return arr;
+  }
+  let el = arr.shift();
+  return CSC.recursiveReverse(arr).concat(el);
 };
 
 })(CSC);
