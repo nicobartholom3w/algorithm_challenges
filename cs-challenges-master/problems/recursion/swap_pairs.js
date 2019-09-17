618029e1f07ffe3a07355dfca5721e73f8f87852
Nicole Bartholomew <nabartholomew@gmail.com>
2019-09-17 10:54:18 -0500

    (feat) Completed initial solution for swap_pairs.

--- cs-challenges-master/problems/recursion/swap_pairs.js
+++ cs-challenges-master/problems/recursion/swap_pairs.js
@@ -13,11 +13,24 @@
 (function(CSC){
 
 CSC.iterativePairSwap = function(arr) {
-
+  for(let i = 0; i < arr.length - 1; i++) {
+    if(arr[i + 1] === undefined) {
+      break;
+    }
+    let temp = arr[i];
+    arr[i] = arr[i + 1];
+    arr[i + 1] = temp;
+    i++;
+  }
+  return arr;
 };
 
 CSC.recursivePairSwap = function(arr) {
-
+  if(arr.length === 0 || arr.length === 1) {
+    return arr;
+  }
+  let pair = [arr[1], arr[0]];
+  return pair.concat(CSC.recursivePairSwap(arr.splice(2)));
 };
 
 })(CSC);
