0bfea94c52a412822880dc2293b65ed502f958d5
Nicole Bartholomew <nabartholomew@gmail.com>
2019-09-18 17:28:09 -0500

    (feat) Completed initial solution for list_sum.

--- cs-challenges-master/problems/recursion/list_sum.js
+++ cs-challenges-master/problems/recursion/list_sum.js
@@ -9,10 +9,18 @@
 (function(CSC){
 
 CSC.iterativeListSum = function(arr) {
-
+  let sum = 0;
+  for(let item of arr) {
+    sum += item;
+  }
+  return sum;
 };
 
 CSC.recursiveListSum = function(arr) {
-
+  if(arr.length === 1) {
+    return arr[0];
+  }
+  let item = arr.shift();
+  return CSC.recursiveListSum(arr) + item;
 };
 })(CSC);
