e4c653c66c20a526d485f7705c0fc03649603ae2
Nicole Bartholomew <nabartholomew@gmail.com>
2019-09-19 19:14:10 -0500

    (feat) Completed initial solution for separate_pairs.

--- cs-challenges-master/problems/recursion/separate_pairs.js
+++ cs-challenges-master/problems/recursion/separate_pairs.js
@@ -13,11 +13,27 @@
 (function(CSC){
 
 CSC.iterativePairSeparate = function(arr) {
-
+  let newArr = [];
+  if(arr.length % 2 !== 0) {
+    arr.push(0);
+  }
+  for(let i = 0; i < arr.length; i++) {
+    let pair = [arr[i], arr[i + 1]];
+    newArr.push(pair);
+    i++;
+  }
+  return newArr;
 };
 
 CSC.recursivePairSeparate = function(arr) {
-
+  if(arr.length === 0) {
+    return arr;
+  }
+  if(arr.length % 2 !== 0){
+    arr.push(0);
+  }
+  let pair = [[arr[arr.length - 2], arr[arr.length - 1]]];
+  return CSC.recursivePairSeparate(arr.slice(0, -2)).concat(pair);
 };
 
 })(CSC);
