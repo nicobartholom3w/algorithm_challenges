69b2d9b3502761ab0e9e5f066b7ca3bdc46ae999
Nicole Bartholomew <nabartholomew@gmail.com>
2019-09-23 18:12:01 -0500

    (feat) Completed initial solution for flatten_list.

--- cs-challenges-master/problems/recursion/flatten_list.js
+++ cs-challenges-master/problems/recursion/flatten_list.js
@@ -9,11 +9,32 @@
 (function(CSC){
 
 CSC.iterativeListFlatten = function(arr) {
-  
+  let newArr = [];
+  for(let i = 0; i < arr.length; i++) {
+    if(!Array.isArray(arr[i])){
+      newArr.push(arr[i]);
+    }
+    else {
+      for(let k = 0; k < arr[i].length; k++){
+        if(!Array.isArray(arr[i][k])){
+          newArr.push(arr[i][k]);
+        }
+      }
+    }
+  }
+  console.log(arr);
+  console.log(newArr);
 };
 
 CSC.recursiveListFlatten = function(arr) {
-
+  if(!Array.isArray(arr)) {
+    return [arr];
+  }
+  else if(arr.length === 1) {
+    return arr[0];
+  }
+  let tempArr = arr.shift();
+  return CSC.recursiveListFlatten(tempArr).concat(CSC.recursiveListFlatten(arr));
 };
 
 })(CSC);
